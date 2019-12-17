b215a0275313d73d71b58beffc125c45e2ee5bf8
Santiago Degetau Sada <santiago.degetau@icloud.com>
2019-12-17 14:25:44 -0600

    Data types (only some)

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod functions;
 mod rust_basics;
 
 fn main() {
-    rust_basics::guess_game::main();
+    rust_basics::data_types::main();
 }
--- src/rust_basics/data_types.rs
+++ src/rust_basics/data_types.rs
@@ -1,6 +1,15 @@
 pub fn main() {
-    
     let x: u32 = "42".parse().expect("Not a number!");
     println!("x = {}", x);
 
+    // Integer Literals
+    let decimal: i32 = 100_000;
+    let hex: i32 = 0xff;
+    let octal: i32 = 0o77;
+    let binary: i32 = 0b1111_0000;
+    println!("{}, {}, {}, {}", decimal, hex, octal, binary);
+
+    // Arrays
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let a = [1i32; 5];
 }
